5d903866fcef912e7ad3bdf9979a4b31e9d05a34
piotr432 <34043820+piotr432@users.noreply.github.com>
2018-12-07 19:22:04 +0100

    Add files via upload

---
+++ PracaDomowa.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int a,b,c;
+int main(){
+cout<<"Podaj 3 liczby: ";
+cin>>a>>b>>c;
+cout<<" Suma liczb: "<<a+b+c<<endl;
+cout<<"Srednia arytmetyczna: "<<double(a+b+c)/3<<endl;
+cout<<"Piotr Bazan";
+return 0;
+}
